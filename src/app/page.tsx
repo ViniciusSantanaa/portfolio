067002e49cb203b6220939af9c093c70e7844c62
Vinicius <viniciussantanna2005@gmail.com>
2024-12-02 16:01:35 -0300

    colocando os cards + imagens dos cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import styles from "@/app/page.module.css"
 import Image from "next/image"
 import imagem_programador from "@/assets/imagem_programador.png"
 import { Card_Skill } from "@/components/card_skills/card_skills"
-import imagem from "@/assets/imagem_freepik.avif"
+import imagem_front from "@/assets/imagem_front_end.jpg"
+import imagem_back from "@/assets/imagem_back_end.png"
+import imagem_data from "@/assets/imagem_data_base.jpg"
 
 export default function Home() {
   return (
@@ -15,12 +17,14 @@ export default function Home() {
         <Image src={imagem_programador} alt={"imagem ilustrativa"} className={styles.imagem_programador}/>
       </section>
 
+      <section>
         <h1 className={styles.h1_Skills}>Skills</h1>
-      <section className={styles.container_skills}>
-        <Card_Skill titulo={"Front End"} skills={"Html, CSS, Javascript, React, Next"} imagem={imagem} />
-        <Card_Skill titulo={"Front End"} skills={"Html, CSS, Javascript, React, Next"} imagem={imagem} />
-        <Card_Skill titulo={"Front End"} skills={"Html, CSS, Javascript, React, Next"} imagem={imagem} />
-        <Card_Skill titulo={"Front End"} skills={"Html, CSS, Javascript, React, Next"} imagem={imagem} />
+        <div className={styles.div_skills}>  
+          <Card_Skill titulo={"Front End"} skills={"Html, CSS, Javascript, React, Next.js"} imagem={imagem_front} />
+          <Card_Skill titulo={"Back end"} skills={"Python"} imagem={imagem_back} />
+          <Card_Skill titulo={"Data-Base"} skills={"Oracle"} imagem={imagem_data} />
+          <Card_Skill titulo={"Ferramentas"} skills={"Git"} imagem={imagem_front} />
+        </div>
       </section>
 
     </main>
